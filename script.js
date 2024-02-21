98f4be1b9f5efc51260ed107a46f903a075c3e48
Jufarkas <139999007+Jufarkas@users.noreply.github.com>
2024-02-20 21:25:28 -0800

    add eventlstnr and temp confirmation alert for dialog box. Commented out functions are WIP

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
-const readBtn = document.querySelector('.readToggle');
-const deleteBtn = document.querySelector('.delete');
+const readBtn = document.querySelectorAll('.readToggle')
+const deleteBtn = document.querySelectorAll('.delete');
+const dialog = document.querySelector('dialog');
+const addNewBook = document.querySelector('.showDialog');
+const submit = document.querySelector('.formConfirm');
+let title;
+let author;
+let pages;
+let hasRead;
 
-readBtn.addEventListener('click', () => {
 
+addNewBook.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+submit.addEventListener('click', () => {
+    alert ("clicked");
 })
 
 
-deleteBtn.addEventListener('click', () => {
+// change 'read' status
 
-})
+// readBtn.forEach(function(button){
+//     button.addEventListener('click', () => {
+//         let readStatus = document.querySelectorAll('.hasRead');
+//         if(hasRead.textContent === "Status:"){
+//             readStatus.textContent = "Status: read";
+//             button.textContent = "Mark as unread"
+//         } else if (readStatus.textContent === "Status: read") {
+//             readStatus.textContent = "Status: not yet read";
+//             button.textContent = "Mark as read";
+//         } else {
+//             readStatus.textContent = "Status: read";
+//             button.textContent = "Mark as unread";
+//         }
+//     })
+// });
+
+
+// deleteBtn.addEventListener('click', () => {
+//     let bookList = document.querySelector('.book-list')
+//     bookList.removeChild()
+// })
 
 
 
@@ -20,11 +52,11 @@ function Book(title, author, pages, hasRead) {
     this.author = author;
     this.pages = pages;
     this.hasRead = hasRead;
-    // have 3 options "read" "not read" "currently reading"
+    // have 2 options "read" "not read"
 }
 
 // placeholder books for now to help build around
-const book1 = new Book('IT', 'Stephen King', 1138, 'currently reading');
+const book1 = new Book('IT', 'Stephen King', 1138, 'read');
 const book2 = new Book('Frankenstein; or, The Modern Prometheus', 'Mary Shelley', 280, 'read');
 const book3 = new Book('Stories of Your Life and Others', 'Ted Chiang', 333, 'not read');
 const book4 = new Book('The Curious Incident of the Dog in the Night-Time', 'Mark Haddon', 274, 'read');
